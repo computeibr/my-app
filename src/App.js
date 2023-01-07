7fabc47bd6257dd38db2c9afb00fc18f3bc83afc
Leonardo Pires <leonardo.computei@gmail.com>
2023-01-07 13:15:07 -0300

    sbadmin2 exemplo camera

--- src/App.js
+++ src/App.js
@@ -639,7 +639,7 @@ function App() {
                                 <div className="card-body">
                                     <div className="text-center">
                                         <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: "25rem;"}}
-                                            src="img/undraw_posting_photo.svg" alt="..." />
+                                            src="http://187.111.99.18:9004/?CODE=591" alt="..." />
                                     </div>
                                     <p>Add some quality, svg illustrations to your project courtesy of <a
                                             target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a
